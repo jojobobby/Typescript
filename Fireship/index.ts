9b7f27c8eded26c6c53aa6b70c6d5fa2b7f3f07e
Jojobobby <raphealsmall@gmail.com>
2023-05-22 15:01:37 -0500

    Basics

---
+++ Fireship/index.ts
@@ -0,0 +1,51 @@
+//https://www.youtube.com/watch?v=ahCwqrYpIuM
+
+//variable types
+let lucky;
+
+type Style = 'bold' | 'italic';
+let number:Style = "bold";
+
+
+//interfaces
+interface Person {
+    first: "",
+    last: ""
+}
+
+const person: Person = {
+    first: '',
+    last: ''
+}
+
+//function typing
+function pow(x:number, y:number) : string {
+    return Math.pow(x,y).toString();
+}
+
+pow(5, 5);
+
+//array typing
+const array: string[] = []
+array.push(pow(3, 3))
+array.push(pow(3, 3))
+
+
+//more interface
+interface MyListInterface {
+    listItem1:0
+    listItem2:1
+}
+
+const objectArray : MyListInterface[] = []
+
+//optional types with ? and array containing objects
+type MyList = [number?, string?, boolean?]
+
+const superArray : MyList[] = []
+
+
+//Generics
+class Observable<T> {
+    constructor(public value: T) {}
+}
